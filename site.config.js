b4eb1702a3b4804015abc2e9d81ec8ce233f1dfe
bricyma <brightmzb@gmail.com>
2022-01-23 13:24:07 -0800

    update domain

--- site.config.js
+++ site.config.js
@@ -7,9 +7,9 @@ module.exports = {
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'just documentations',
+  domain: 'oldmaru.vercel.app',
+  author: 'old maru',
 
   // open graph metadata (optional)
   description: 'Example site description',
